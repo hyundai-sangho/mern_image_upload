beb97ecb9e49f2ae6b6b138ac9659e68945b546f
조상호 <hyundai_sangho@naver.com>
2025-05-23 17:06:23 +0900

    uploadForm 추가가

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const UploadForm = () => {
+	const [file, setFile] = useState(null);
+
+	useEffect(() => {
+		console.log('useEffect : ', { file });
+	}, [file]);
+
 	return (
 		<form>
 			<label htmlFor='image'>사진</label>
-			<input id='image' type='file' />
+			<input
+				id='image'
+				type='file'
+				onChange={(event) => {
+					const imageFile = event.target.files[0];
+					if (imageFile) {
+						setFile(imageFile);
+					} else {
+						setFile(null);
+					}
+				}}
+			/>
 			<button type='submit'>제출</button>
 		</form>
 	);
